0e5994fcb5cc7e3a81d17090ac7340ad8d12d28e
Akshitha <2407akshi@gmail.com>
2023-10-04 21:04:07 +0530

    localhostremoved

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -6,7 +6,7 @@ function Login(){
     async function submit(){
         const email = (document.getElementById("email") as HTMLInputElement).value
         const password = (document.getElementById("password") as HTMLInputElement).value
-        const response = await fetch('http://localhost:4000/api/login',
+        const response = await fetch('/api/login',
         {
             method:"POST",
             headers: {
